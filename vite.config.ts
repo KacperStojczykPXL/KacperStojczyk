7cbee459b65800dea89621d263f01ab292cc432c
Kacper Stojczyk <73295729+KacperStojczykPXL@users.noreply.github.com>
2025-04-11 11:22:48 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // Replace 'portfolio' with your repository name
+  base: '/KacperStojczyk/', 
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
